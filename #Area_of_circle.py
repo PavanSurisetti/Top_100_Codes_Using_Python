45255bb0112573f3da42a963380db15568c2909a
PavanSurisetti <pavan1898235@gmail.com>
2025-01-15 13:04:49 +0530

    [code-46] Area of a Circle

---
+++ #Area_of_circle.py
@@ -0,0 +1,6 @@
+#Area_of_circle
+radius=float(input("Enter the Radius of the Circle:"))
+def area(r):
+    return(3.14*(r**2))
+Area=area(radius)
+print(f'The Area of The circle for the given radius is:{Area}')
