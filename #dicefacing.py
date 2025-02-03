4d70dacca60360b7e41e584c5e0755e3dfa9d133
PavanSurisetti <pavan1898235@gmail.com>
2025-02-03 22:13:35 +0530

    [code-63] Opposite face a dice

---
+++ #dicefacing.py
@@ -0,0 +1,14 @@
+#dicefacing
+face=int(input("Enter a face:"))
+if(face==1):
+    print(f'the oppsoite face of {face} is 6')
+elif(face==2):
+    print(f'the oppsoite face of {face} is 5')
+elif(face==3):
+    print(f'the oppsoite face of {face} is 4')
+elif(face==4):
+    print(f'the oppsoite face of {face} is 3')
+elif(face==5):
+    print(f'the oppsoite face of {face} is 2')
+else:
+    print(f'the oppsoite face of {face} is 1')
