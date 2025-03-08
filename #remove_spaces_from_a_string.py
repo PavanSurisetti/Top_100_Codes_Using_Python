3a859f2e292c92f3567acea01ea4c2867720d083
PavanSurisetti <pavan1898235@gmail.com>
2025-03-08 22:53:49 +0530

    [code-90]  Remove Spaces from a string

---
+++ #remove_spaces_from_a_string.py
@@ -0,0 +1,4 @@
+#remove_spaces_from_a_string
+str=input("Enter a String:")
+print(f'Before Remving Spaces:{str}')
+print(f'After Removing Spaces:{str.replace(' ','')}')#replace syntax is .replace(existing_character,new_character)
