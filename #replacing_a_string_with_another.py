38c798460b4885fd209db10867299ab1baae3a76
PavanSurisetti <pavan1898235@gmail.com>
2025-03-09 11:53:14 +0530

    [code-97] Replacing string with another string

---
+++ #replacing_a_string_with_another.py
@@ -0,0 +1,6 @@
+#replacing_a_string_with_another
+str=input('Enter String:')
+specific_word=input('Enter a Specific word to replace:')
+replace_string=input("Enter a string to  Replace String:")
+print(f"Before Replacing :{str}")
+print(f"After Replacing :{str.replace(specific_word,replace_string)}")
